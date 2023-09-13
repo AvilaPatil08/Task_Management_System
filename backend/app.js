516bd564a71f787f991f0730cd0ebe5dc1cd5ce5
AvilaPatil08 <144425511+AvilaPatil08@users.noreply.github.com>
2023-09-13 10:40:44 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,23 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+// Middleware
+app.use(bodyParser.json());
+
+// MongoDB Connection
+mongoose.connect("mongodb://localhost/taskmanager", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+mongoose.connection.on("error", (error) => console.error(error));
+mongoose.connection.once("open", () => console.log("Connected to MongoDB"));
+
+// Start Server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 
